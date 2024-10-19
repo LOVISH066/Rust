a6f605f8e254ee4a6a61af1ea7b995d897b51a47
LOVISH GOYAL <112848018+LOVISH066@users.noreply.github.com>
2024-10-20 02:13:05 +0530

    Update Fibbonacci_Series.rs

--- Fibbonacci_Series.rs
+++ Fibbonacci_Series.rs
@@ -1,21 +1,21 @@
 
 fn main() {
-    let x: i32 = 1;
-    println!("{}", x);
+    let x: u32 = 4;
+    println!("{}", fib(x));
 }
 
-fn fib(num: i32) -> i32 {
+fn fib(num: u32) -> u32 {
     let mut first = 0;
     let mut second = 1;
-    if(num == 0)
+    if num == 0 
     {
         return first;
     }
-    if(num == 1)
+    if num == 1
     {
         return second;
     }
-    for i in 1..num - 2 {
+    for _ in 0..(num - 2) {
         let temp = second;
         second = second + first;
         first = temp;
