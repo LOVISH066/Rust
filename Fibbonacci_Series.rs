c5ee13a1086f9bf38078e1b45ea7c7c625293682
LOVISH GOYAL <112848018+LOVISH066@users.noreply.github.com>
2024-10-20 02:07:21 +0530

    Create Fibbonacci_Series.rs

---
+++ Fibbonacci_Series.rs
@@ -0,0 +1,24 @@
+
+fn main() {
+    let x: i32 = 1;
+    println!("{}", x);
+}
+
+fn fib(num: i32) -> i32 {
+    let mut first = 0;
+    let mut second = 1;
+    if(num == 0)
+    {
+        return first;
+    }
+    if(num == 1)
+    {
+        return second;
+    }
+    for i in 1..num - 2 {
+        let temp = second;
+        second = second + first;
+        first = temp;
+    }
+    return second;
+}
